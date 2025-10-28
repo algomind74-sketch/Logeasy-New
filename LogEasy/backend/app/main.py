9728739bfc4655b2bd2af7acd0cad45fe15a0d89
AlgoMind <algomind74@gmail.com>
2025-10-28 20:17:57 +0530

    dccccc

--- LogEasy/backend/app/main.py
+++ LogEasy/backend/app/main.py
@@ -12,9 +12,7 @@ app = FastAPI(
 # ✅ Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000",
-        "http://localhost:3001",
-        "http://localhost:3002"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
